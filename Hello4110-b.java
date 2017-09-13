2e5b0131f24d8e54d9c619c717651ec19806156b
Ashley Roselius <AshleyRoselius@Ashleys-MacBook.local>
2017-09-12 20:59:29 -0400

    add three files but nothing else allowed through

--- Hello4110-b.java
+++ Hello4110-b.java
@@ -1,5 +1,5 @@
-class Hello4110{
-	public static void main(String[] args){
+class Hello4110b {
+	public static void main(String[] args) {
 		System.out.println("Hello 4110! Welcome to class!");
 	}
 }
